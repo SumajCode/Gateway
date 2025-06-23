42451ae866294128966c41fed97374df66bd18e3
raulpsl <raulparedesld@gmail.com>
2025-06-23 10:34:54 -0400

    update: repainr for routes in contenido

--- src/infra/routes/RoutesContenido.ts
+++ src/infra/routes/RoutesContenido.ts
@@ -2,14 +2,14 @@ import { generarRutasProxy } from "../../features/secure/proxys/ProxyRoutes";
 import { RutaProxy } from "../../shared/interfaces/ProxyInterface";
 
 const rutasContenido: RutaProxy[] = [
-    { method: 'delete', route: '/archivo/crear', destiny: '/archivo/crear'},
-    { method: 'get', route: '/archivo/editar', destiny: '/archivo/editar'},
-    { method: 'post', route: '/archivo/eliminar', destiny: '/archivo/eliminar'},
-    { method: 'patch', route: '/archivo/listar', destiny: '/archivo/listar'},
-    { method: 'delete', route: '/modulo/crear', destiny: '/modulo/crear'},
-    { method: 'get', route: '/modulo/editar', destiny: '/modulo/editar'},
-    { method: 'post', route: '/modulo/eliminar', destiny: '/modulo/eliminar'},
-    { method: 'patch', route: '/modulo/listar', destiny: '/modulo/listar'},
+    { method: 'post', route: '/archivo/crear', destiny: '/archivo/crear'},
+    { method: 'patch', route: '/archivo/editar', destiny: '/archivo/editar'},
+    { method: 'delete', route: '/archivo/eliminar', destiny: '/archivo/eliminar'},
+    { method: 'get', route: '/archivo/listar', destiny: '/archivo/listar'},
+    { method: 'post', route: '/modulo/crear', destiny: '/modulo/crear'},
+    { method: 'patch', route: '/modulo/editar', destiny: '/modulo/editar'},
+    { method: 'delete', route: '/modulo/eliminar', destiny: '/modulo/eliminar'},
+    { method: 'get', route: '/modulo/listar', destiny: '/modulo/listar'},
     { method: 'get', route:'/', destiny: '/'}
 ]
 
